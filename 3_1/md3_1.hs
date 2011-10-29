a6c0e596cecea9d17fcb74a8c6ee668d7a41a494
Dainis Tillers <dainis.tillers@gmail.com>
2011-10-29 18:49:05 +0300

    added: task 3_1

---
+++ 3_1/md3_1.hs
@@ -0,0 +1,22 @@
+module Md where
+
+find (x, []) = []
+find (x, (bh:bt)) = if fst(bh) == x then snd(bh):find(x, bt) else find (x, bt)
+
+translate [] b = []
+translate (ah:at) b = if translation /= [] then (map (\x-> (fst(ah), x)) translation)++translate at b else (translate at b)
+    where translation = find (snd(ah), b)
+
+
+exists x [] = False;
+exists x (h:t) = if x == h then True else exists x t
+
+
+unique [] = [];
+unique (h:t) = if not(exists h t) then h:unique t else unique t
+
+
+perform_translation a b = unique (translate a b)
+
+test1 = perform_translation [("a", "b"), ("b", "b"), ("c", "b"), ("d", "b"), ("e", "b")] [("b","a"), ("b","b"), ("b", "c"), ("b", "d"), ("b", "e")]
+test2 = perform_translation [("a", "a"), ("b", "b"), ("c", "c"), ("d", "d")] [("a", "a"), ("b", "b"), ("c", "c"), ("d", "d"), ("a", "a"), ("b", "b"), ("c", "c"), ("d", "d")]
