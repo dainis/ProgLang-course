59eea2f848e5cd41859ce44e0cafb259a4d48fb8
Dainis Tillers <dainis.tillers@gmail.com>
2011-11-14 21:51:49 +0200

    modified: better comments and doc

--- 4_2/md4_2.pl
+++ 4_2/md4_2.pl
@@ -1,4 +1,4 @@
-% salîdzinâðanas predikâti
+% salîdzinâðanas predikâti, tiek izmantoti, jo termini ir salikti no 3 daþâdâm vçrtîbâm
 cmp_1((_, X, _), (_, Y, _)) :- X =< Y, !.
 cmp_2((_, X, _), (_, Y, _)) :- X > Y, !.
 
@@ -16,15 +16,16 @@ min_v(A, X) 		:- min_list(A, X).
 max_v([], X) 		:- X is 0.
 max_v(A, X) 		:- max_list(A, X).
 
-% koka apstaigâðanas funkcijas
-aa(n0(A), X) 		:- min_v(A, Min), max_v(A, Max), length(A, Len), append([(Len, Min, Max)], [], X).
+% koka apstaigâðanas funkcijas, ja virsotne ir lapa vai arî ar vienu bçrnu tad rezultâtam nonâkot tajâ, tas ir jau sakârtots
+aa(n0(A), X) 		:- min_v(A, Min), max_v(A, Max), length(A, Len), X = [(Len, Min, Max)]. % atrod minâlo, maksimâlo vçrtîbu un saraksta garumumu
 aa(n1(T1), X) 		:- aa(T1, Y), append(Y, [], X).
-aa(n2(T1, T2), X) 	:- aa(T1, Y), aa(T2, Z), merge(Y, Z, X).
+aa(n2(T1, T2), X) 	:- aa(T1, Y), aa(T2, Z), merge(Y, Z, X). % tâ kâ ðajâ gadîjumâ ir divi rezultâti tad tie ir jâsavieno kopâ, lai rezultâts bûtu sakârtots
 
-% veic katra saraksta elementa kâpinâðanu kvadrâtâ
+% veic katra saraksta elementa kâpinâðanu kvadrâtâ, darbs notiek rekursîvi
 double_list([], X)	:- X = [].
 double_list([Xh | Xt], X)	:- Y is Xh * Xh, double_list(Xt, Z), append([Y], Z, X).
 
+% veic koka apstaigâðanu un rekonstruçðanu
 bb(n0(A), X)		:- double_list(A, Y), X = n0(Y).
 bb(n1(T1), X) 		:- bb(T1, Y), X = n1(Y).
 bb(n2(T1, T2), X) 	:- bb(T1, Y), bb(T2, Z), X = n2(Y, Z).
